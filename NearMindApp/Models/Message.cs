33f334821f9cc90ef62e12996fd3630b274a3836
vic0rv <victor30072002@hotmail.com>
2024-11-08 14:29:05 +0100

    requires atributos messages.cs

--- NearMindApp/Models/Message.cs
+++ NearMindApp/Models/Message.cs
@@ -8,8 +8,8 @@ namespace NearMindApp.Models
 {
     public class Message
     {
-        public string User { get; set; }
-        public string Text { get; set; }
+        public required string User { get; set; }
+        public required string Text { get; set; }
         public DateTime Timestamp { get; set; }
     }
 }
