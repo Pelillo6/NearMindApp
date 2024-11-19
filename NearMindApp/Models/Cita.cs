01ea0f7cf10abb2f3b098736b33158dae5534aad
Pelillo6 <javimorenoh6@gmail.com>
2024-11-19 13:28:34 +0100

    38 errores para ti

---
+++ NearMindApp/Models/Cita.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Supabase.Postgrest.Models;
+
+namespace NearMindApp.Models
+{
+    public class Cita : BaseModel
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [JsonPropertyName("fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [JsonPropertyName("psicologo_id")]
+        public Guid PsicologoId { get; set; }
+
+        [JsonPropertyName("psicologo")]
+        public Psicologo Psicologo { get; set; }
+
+        [Required]
+        [JsonPropertyName("paciente_id")]
+        public Guid PacienteId { get; set; }
+
+        [JsonPropertyName("paciente")]
+        public Paciente Paciente { get; set; }
+
+        public Cita()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ NearMindApp/Models/Psicologo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace NearMindApp.Models
+{
+    public class Psicologo : Usuario
+    {
+        public enum EspecialidadEnum
+        {
+            PsicologiaClinica,
+            PsicologiaInfantilYAdolescente,
+            PsicologiaEducativa,
+            PsicologiaOrganizacional,
+            PsicologiaDelDeporte,
+            PsicologiaDeLaSalud,
+            PsicoterapiaFamiliarYPareja,
+            PsicologiaForense,
+            Neuropsicologia,
+            PsicologiaDeLaSexualidad,
+            PsicologiaSocial,
+            TerapiaDeAdicciones,
+            PsicologiaTranspersonal,
+            PsicologiaGerontologica,
+            PsicologiaPositiva
+        }
+
+        [JsonPropertyName("especialidad")]
+        public EspecialidadEnum Especialidad { get; set; }
+    }
+}
