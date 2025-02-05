a19653fa983fe5e182b193753c3800a1055faa5f
vic0rv <victor30072002@hotmail.com>
2025-02-05 14:53:56 +0100

    Pequeños detalles

--- NearMindApp/MauiProgram.cs
+++ NearMindApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using Syncfusion.Licensing;
 using Syncfusion.Maui.Core.Hosting;
 
@@ -9,17 +10,22 @@ namespace NearMindApp
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
+            // Registrar la licencia de Syncfusion
             SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX5ceHRWQmlYUkZ+XUo=");
+
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() // Agregar el Maui Community Toolkit aquí
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                }).ConfigureSyncfusionCore(); 
+                })
+                .ConfigureSyncfusionCore(); // Syncfusion debe estar después de UseMauiCommunityToolkit
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
